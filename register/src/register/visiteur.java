e957916a32072eb7e19eab3b1ac585a4818e6f99
Shamberlin <97014206+Shamberlin@users.noreply.github.com>
2022-01-10 17:03:06 +0100

    commit fait

--- register/src/register/visiteur.java
+++ register/src/register/visiteur.java
@@ -1,5 +1,29 @@
 package register;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class visiteur {
+import javax.swing.JButton;
 
-}
+public class visiteur extends frame implements ActionListener {
+	JButton s;
+	JButton t;
+	panel p;
+	visiteur(){
+		p=new panel();
+	    s=new JButton();
+	    t=new JButton();
+	    t.setText("VENDEUR");
+	    s.setText("CLIENT");
+	    t.setBounds(200,400,100,30);
+	    s.setBounds(500,400,100,30);
+		s.addActionListener(this);
+		t.addActionListener(this);
+		p.add(s);
+		p.add(t);
+		this.add(p);
+		this.setVisible(true);
+		
+		}
